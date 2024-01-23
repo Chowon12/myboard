76f08720947c30ee6d6c1ba02161cd468ca158bc
SeoHyunJo <whtjgus10@gmail.com>
2024-01-23 14:10:29 +0900

    UserDTO 완성

--- src/main/java/com/myboard/shop/dto/User.java
+++ src/main/java/com/myboard/shop/dto/User.java
@@ -1,7 +1,25 @@
 package com.myboard.shop.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
 	private String id;
 	private String password;
 	private Integer author;
+	
+	@Builder
+	public User(String id, String password, Integer author) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.author = author;
+	}	
 }
